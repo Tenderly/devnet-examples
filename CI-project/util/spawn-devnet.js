026fa101e0c7302b21f273d117783c65587b0666
Filip Petrovic <filip.petroviccc@gmail.com>
2023-04-05 09:36:36 +0200

    debug

--- CI-project/util/spawn-devnet.js
+++ CI-project/util/spawn-devnet.js
@@ -3,9 +3,13 @@ dotenv.config();
 
 const {
     TENDERLY_ACCESS_KEY,
+    CI,
 } = process.env;
 
-const command = "./util/tenderly-cli spawn-rpc --project devnet-testing --template devnet-example-ci --account fa4a29af-ad72-44ac-9261-4bf3a8af3a06  --access_key "+ TENDERLY_ACCESS_KEY
+let command = "./util/tenderly-cli spawn-rpc --project devnet-testing --template devnet-example-ci --account fa4a29af-ad72-44ac-9261-4bf3a8af3a06  --access_key "+ TENDERLY_ACCESS_KEY
+if (CI === "true") {
+    command = "./util/tenderly-cli-linux spawn-rpc --project devnet-testing --template devnet-example-ci --account fa4a29af-ad72-44ac-9261-4bf3a8af3a06  --access_key "+ TENDERLY_ACCESS_KEY
+}
 
 const util = require('util');
 const fs = require("fs");
@@ -14,6 +18,8 @@ const exec = util.promisify(require('child_process').exec);
 const createDevNet = async () => {
     const {stderr} = await exec(command);
     const devNetUrl = stderr.trim().toString();
+
+    console.log("DEVNET_RPC_URL=" + devNetUrl)
 
     const fs = require('fs');
     // if file not exists, create it
