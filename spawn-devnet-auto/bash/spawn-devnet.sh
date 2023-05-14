1d103e8c9b8c1d700e3fbcf12b282fd8b30894b7
Filip Petrovic <filip.petroviccc@gmail.com>
2023-05-14 17:57:57 +0200

    updated bash script

--- spawn-devnet-auto/bash/spawn-devnet.sh
+++ spawn-devnet-auto/bash/spawn-devnet.sh
@@ -5,18 +5,22 @@ source ${PWD}/.env
 # Call CLI command and store response in variable
 response=$(tenderly devnet spawn-rpc --template $TENDERLY_DEVNET_TEMPLATE --project $TENDERLY_PROJECT_SLUG 2>&1)
 
-# Set environment variable with response
+# Set an environment variable with the RPC URL response
+# DEVNET_RPC_URL is used to store the RPC URL of the newly spawned devnet
 export DEVNET_RPC_URL="$response"
 
-# Replace existing environment variable in .zshrc file
-if grep -q "export DEVNET_RPC_URL=" ~/.zshrc; then
-  sed -i.bak "s|export DEVNET_RPC_URL=.*|export DEVNET_RPC_URL=\"$DEVNET_RPC_URL\"|" ~/.zshrc
-  rm ~/.zshrc.bak
+# Replace the existing DEVNET_RPC_URL environment variable in the .zshrc file
+# If it already exists, replace it with the new value; if not, add it to the file
+if grep -q "export DEVNET_RPC_URL=" $HOME/.zshrc; then
+  sed -i.bak "s|export DEVNET_RPC_URL=.*|export DEVNET_RPC_URL=\"$DEVNET_RPC_URL\"|" $HOME/.zshrc
+  rm $HOME/.zshrc.bak
 else
-  echo "export DEVNET_RPC_URL=\"$DEVNET_RPC_URL\"" >> ~/.zshrc
+  # The following environment variable is used to store the RPC URL of the newly spawned devnet
+  echo "# DEVNET_RPC_URL is used to store the RPC URL of the newly spawned devnet" >> $HOME/.zshrc
+  echo "export DEVNET_RPC_URL=\"$DEVNET_RPC_URL\"" >> $HOME/.zshrc
 fi
 
-# Reload .zshrc file
-source ~/.zshrc
+# Reload the .zshrc file to use the new environment variable
+source $HOME/.zshrc
 
 echo "Successfully spawned devnet with RPC URL: $DEVNET_RPC_URL"
